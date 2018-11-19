81c8ef9ddc1e13c1c70ca5e9e27ca9b6f3c53c34
Jef De CONTI <jefdc05@gmail.com>
2018-11-19 13:59:10 +0100

    add converteur

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Category;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="article_show")
+     */
+    public function show(Category $category) :Response
+    {
+        return $this->render('category/category.html.twig', ['category'=>$category]);
+    }
+}
